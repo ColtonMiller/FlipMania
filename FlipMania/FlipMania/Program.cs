4e4f5cadf36921cd5eece058d58239ca38def08d
Ryan Wilson <ryanmitchellwilson@gmail.com>
2015-04-07 20:51:47 -0600

    Started FlipCoins function
    Began writing the function to flip a coin

--- FlipMania/FlipMania/Program.cs
+++ FlipMania/FlipMania/Program.cs
@@ -8,8 +8,23 @@ namespace FlipMania
 {
     class Program
     {
+
+        Random rng = new Random();
+
         static void Main(string[] args)
         {
         }
+
+        public static void FlipCoins(int numberOfFlips)
+        {
+            int numberOfHeads = 0;
+            int numberOfTails = 0;
+
+            for (int i = 0; i <= numberOfFlips; i++)
+            {
+                
+            }
+        }
+
     }
 }
